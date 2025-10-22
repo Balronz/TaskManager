663d5ab6c249537bc34ed881b1e2690f8c382bbc
Balronz <mateobalciunas@gmail.com>
2025-10-22 21:36:58 +0200

    Creacion del modelo User con: propiedades, validador email, hash contraseña, validador/comparador contraseña

--- server/models/user.js
+++ server/models/user.js
@@ -1,43 +1,78 @@
-/** 
- * 
- * El modelo de usuario define la estructura que tendra cada usuario de la BBDD
-**/
-
+//Importar bibliotecas
 import mongoose from "mongoose";
 import date from "date-and-time";
+import validator from "validator"; //Validador email
+import bcrypt from "bcryptjs"; //Encriptar contraseñas
 
 
-const user = new mongoose.Schema({
+//Crear la clase User
 
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
+    
     email: {
         type: String,
         required: true,
         unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        lowercase: true,
+        validate: (value) => { //Validador de email, hace uso de la libreria y del metodo validator.IsEmail(emailValor)
+            return validator.isEmail(value);
+        }
     },
+
     password: {
         type: String,
         required: true,
+        minlength: 8
     },
+
     role: {
         type: String,
         required: true,
-        enum: ['admin', 'user'],
-        default: 'user'
+        enum: ["user", "admin"],
+        default: "user"
     },
-    creationDate: {
-        type: date,
+    
+    createdAt: {
+        type: Date,
         default: Date.now
     }
 });
 
+//Hook para encriptar password
+userSchema.pre('save', async function(next) {
+    try {
+        //comprobar si se ha modificado la contraseña
+        if(!this.isModified('password')) return next();
 
-//Crear el modelo de usuario
+        //Generar salr round y el hash de password
+        const saltRounds = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, saltRounds);
+
+        next(); //Se pasa al siguiente middleware, guardar(save)
+    }
+    catch (error){
+        console.error(error);
+    }
+});
+
+//Validador/comparador de contraseñas(password)
+
+userSchema.methods.isValidPassword = async function(password) {
+    try{
+        //Comprobar la password escrita con la hasheada y guardada
+        return await bcrypt.compare(password, this.password);
+    }
+    catch(error){
+        throw new Error('Error al comprobar la password');
+    }
+}
+
+//Generar el modelo User
 const User = mongoose.model('User', userSchema);
 
-//Exportar el modelo de usuario
+//Exportar User para poder usarlo en toda la app
 export default User;
